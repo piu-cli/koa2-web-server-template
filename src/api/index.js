2c65a65845300f14b5cd618b0e23729a487e0bcb
luoquanquan <luoquanquan@baidu.com>
2018-10-29 12:50:38 +0800

    代码格式调整

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: luoquanquan
  * @Date: 2018-10-26 15:57:56
- * @Last Modified by: 罗圈圈
- * @Last Modified time: 2018-10-26 23:36:33
+ * @Last Modified by: luoquanquan
+ * @Last Modified time: 2018-10-29 11:56:34
  */
 
 const router = require('koa-router')()
@@ -10,6 +10,6 @@ const demo = require('./demo')
 
 router.prefix('/api')
 
-router.use('/demo', demo.routes(), demo.allowedMethods() )
+router.use('/demo', demo.routes(), demo.allowedMethods())
 
 module.exports = router
